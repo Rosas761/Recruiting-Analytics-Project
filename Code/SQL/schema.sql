cb9c1ebe818698dd2a15346c16e10ec552d7cc8c
Stefan <Rosas761@gmail.com>
2019-10-27 18:20:55 -0400

    commited

---
+++ Code/SQL/schema.sql
@@ -0,0 +1,32 @@
+
+
+
+
+CREATE TABLE EmployeeTable (
+AIdent INT Primary key,
+Activation Day Date,
+Branch Varchar,
+City Varchar,
+How Heard Of varchar,
+
+);
+
+select * from AssignmentTable;
+CREATE TABLE Week2 (
+Performance Code varchar,
+AIdent INT Primary key,
+Start Day date,
+End day date,
+Pay Rate float,
+Bill Rate float,
+Margin float,
+
+);
+
+
+CREATE TABLE Hours Worked (
+Aident INT Primary key,
+Hours Worked  INT,
+
+);
+
