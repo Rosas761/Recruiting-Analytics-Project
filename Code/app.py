e60ef8c9d4d7f49fe6c195a5fb84e5e0d8fce55a
Stefan <Rosas761@gmail.com>
2019-11-26 20:44:34 -0500

    it works

--- Code/app.py
+++ Code/app.py
@@ -23,24 +23,21 @@ cursor = conn.cursor()
 
 app = Flask(__name__)
 
-sql = "select * from snider"
-cursor.execute(sql)
-
-for row in cursor.fetchall():
-    my_data = []
-    print(row)
-    my_data.append(row)
-
-df = pd.DataFrame(my_data)
-table1 = df.rename(columns={0: "item_id", 1: 'aident',2: 'activation',3: 'completion',4: 'pay_rate',5: 'bill_rate',6: 'margin',7: 
-                            'hours_worked',8: 'branch',9: 'city',10: 'how_heard',11: "Revanue"})
-table1
-
-
 
 @app.route("/websites/<website>")
 def websites(website):
-   
+    
+    sql = f"select sum(profit), how_heard from snider where How_Heard = '{website}' group by how_heard order by sum(profit) desc"
+    cursor.execute(sql)
+    for row in cursor.fetchall():
+        my_data = []
+        print(row)
+        my_data.append(row)
+
+    df = pd.DataFrame(my_data)
+    table1 = df.rename(columns={0: "Revanue", 1: "How_Heard"})
+    table1
+    
 
     print(table1)
 return jsonify(table1)
@@ -50,10 +47,7 @@ return jsonify(table1)
 
 
 
-@app.route("/")
-def index():
-    """Return the homepage."""
-    return render_template("index.html")
+
 
 
 
