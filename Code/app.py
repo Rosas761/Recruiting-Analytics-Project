4d45fee5991d91ed1ce7307a9613f88652cba358
Stefan <Rosas761@gmail.com>
2019-11-26 19:34:37 -0500

    dataframe

--- Code/app.py
+++ Code/app.py
@@ -14,20 +14,16 @@ from sqlalchemy import create_engine
 from flask import Flask, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 
-#url = "postgressql://postgres:postgres@localhost:5432/SB_Project" 
+#url = "postgressql://postgres:postgres@localhost:5432/SB_Project"
 #conn = psycopg2.connect(url)
-conn = psycopg2.connect(dbname="SB_Project", user= "postgres", password="postgres", host="localhost", port = "5432")
+conn = psycopg2.connect(dbname="SB_Project", user="postgres",
+                        password="postgres", host="localhost", port="5432")
 
 cursor = conn.cursor()
 
 app = Flask(__name__)
 
 sql = "select sum(profit), how_heard from snider group by how_heard order by sum(profit) desc"
-
-cursor.execute(sql)
-for row in cursor.fetchall():
-    print(row)
-
 
 
 @app.route("/")
@@ -36,17 +32,21 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/names")
-def names():
-    """Return a list of sample names."""
+@app.route("/metadata/<sample>")
+def sample_metadata(sample):
 
-    # Use Pandas to perform the sql query
-    stmt = db.session.query(Samples).statement
-    df = pd.read_sql_query(stmt, db.session.bind)
 
-    # Return a list of the column names (sample names)
-    return jsonify(list(df.columns)[2:])
+my_data = []
+for row in cursor.fetchall():
+    print(row)
+    my_data.append(row)
 
+df = pd.DataFrame(my_data)
+table1 = df.rename(columns={0: "Revanue", 1: "How_Heard"})
+table1
+
+print(sample_metadata)
+return jsonify(sample_metadata)
 
 if __name__ == "__main__":
     app.run()
